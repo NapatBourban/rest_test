08e8055338f06a7f9403d3bb0dc9a6305da616d0
NapatBourban <napatone123@gmail.com>
2024-02-16 12:11:32 +0700

    pm test

--- server.js
+++ server.js
@@ -5,10 +5,7 @@ const app = express()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-app.put('/books/:id', (req, res) => {
-    const updateIndex = books.findIndex(book => book.id === req.params.id)
-    res.json(Object.assign(books[updateIndex], req.body))
-  })
+
 
 app.get('/', (req, res) => {
   res.send('Hello World')
@@ -23,16 +20,22 @@ app.get('/books/:id', (req, res) => {
     res.json(books.find(book => book.id === req.params.id))
 })
 
+app.put('/books/:id', (req, res) => {
+    const updateIndex = books.findIndex(book => book.id === req.params.id);
+    res.json(Object.assign(books[updateIndex], req.body))
+})
+
 app.delete('/books/:id', (req, res) => {
     const deletedIndex = books.findIndex(book => book.id === req.params.id)
-    delete books[deleteIndex];
+    delete books[deletedIndex];
     res.status(200).json(req.body)
- })
+})
 
 app.listen(3000, () => {
   console.log('http://localhost:3000')
 })
 app.post('/books', (req, res) => {
     books.push(req.body)
+    res.status(200).json(req.body)
   })
 
